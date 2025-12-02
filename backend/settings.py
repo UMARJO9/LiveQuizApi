d3dd70bea526ada16537bd1951a316a25adfbc55
UMARJO9 <umarnurmadov12@gmail.com>
2025-12-02 22:48:37 +0500

    fix

--- backend/settings.py
+++ backend/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'live_quiz',
         'USER': 'postgres',
-        'PASSWORD': '', 
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
